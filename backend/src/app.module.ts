fd107e960f9a530f2251478c64127b4dcaf1843f
Syed Muhammed Hassan Ali <syed007hassan@gmail.com>
2023-07-28 17:51:50 +0500

    auth and guard added

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,12 +4,14 @@ import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
 import { ProductModule } from './product/product.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.MONGODB_URI),
     ProductModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
