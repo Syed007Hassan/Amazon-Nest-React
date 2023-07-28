b96419e2eb5bc380b452a0e28f181cec8bdf49b6
Syed Muhammed Hassan Ali <syed007hassan@gmail.com>
2023-07-28 18:34:48 +0500

    userService injected

--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UserService {
+  constructor(@InjectModel('User') private readonly userModel: Model<User>) {}
+
   create(createUserDto: CreateUserDto) {
     return 'This action adds a new user';
   }
