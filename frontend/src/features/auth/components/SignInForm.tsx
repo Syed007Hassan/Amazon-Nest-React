ef9c747d0d06705d8a97de8567ce50d2779a6c27
hassan-ali-OTS <hassan.ali@onetechnologyservices.com>
2023-08-03 17:24:44 +0500

    signin component fixed

--- frontend/src/features/auth/components/SignInForm.tsx
+++ frontend/src/features/auth/components/SignInForm.tsx
@@ -18,114 +18,118 @@ const SignInForm: FC = () => {
   };
 
   return (
-    <Box
-      sx={{
-        border: 1,
-        padding: 2,
-        borderColor: "#cccccc",
-        width: "350px",
-        marginTop: 2,
-        backgroundColor: "#ffffff",
-        borderRadius: "4px",
-        marginBottom: "16px",
-      }}
-    >
-      <form onSubmit={onSubmitHandler}>
-        <Grid container direction={"column"} justifyContent={"flex-start"}>
-          <Typography
-            variant="h5"
-            component={"h4"}
-            color="initial"
-            sx={{ fontFamily: "Amazon Ember, sans-serif" }}
-          >
-            Sign In
-          </Typography>
-          <TextField
-            type="email"
-            sx={{
-              borderColor: "black",
-              fontWeight: 500,
-              marginTop: 2,
-              marginBottom: 1,
-              color: "black",
-            }}
-            id="email"
-            label="Email"
-            variant="outlined"
-          />
-          <TextField
-            type="password"
-            sx={{
-              borderColor: "black",
-              fontWeight: 500,
-              marginTop: 2,
-              marginBottom: 1,
-              color: "black",
-            }}
-            id="email"
-            label="Password"
-            variant="outlined"
-            placeholder="At least 8 characters"
-          />
+    <>
+      <Box
+        sx={{
+          border: 1,
+          padding: 2,
+          borderColor: "#cccccc",
+          width: "350px",
+          marginTop: 2,
+          backgroundColor: "#ffffff",
+          borderRadius: "4px",
+          marginBottom: "16px",
+        }}
+      >
+        <form onSubmit={onSubmitHandler}>
+          <Grid container direction={"column"} justifyContent={"flex-start"}>
+            <Typography
+              variant="h5"
+              component={"h4"}
+              color="initial"
+              sx={{ fontFamily: "Amazon Ember, sans-serif" }}
+            >
+              Sign In
+            </Typography>
+            <TextField
+              type="email"
+              sx={{
+                borderColor: "black",
+                fontWeight: 500,
+                marginTop: 2,
+                marginBottom: 1,
+                color: "black",
+              }}
+              id="email"
+              label="Email"
+              variant="outlined"
+            />
+            <TextField
+              type="password"
+              sx={{
+                borderColor: "black",
+                fontWeight: 500,
+                marginTop: 2,
+                marginBottom: 1,
+                color: "black",
+              }}
+              id="email"
+              label="Password"
+              variant="outlined"
+              placeholder="At least 8 characters"
+            />
 
+            <Button
+              variant="contained"
+              type="submit"
+              sx={{
+                marginTop: "16px",
+                height: "31px",
+                backgroundColor: "#f0c14b",
+                color: "#111111",
+                fontWeight: 700,
+                borderColor: "#a88734 #9c7e31 #846a29",
+                textTransform: "none",
+                fontSize: "14px",
+                borderRadius: "4px",
+                "&:hover": {
+                  backgroundColor: "#e3a600",
+                },
+              }}
+            >
+              Register
+            </Button>
+          </Grid>
+        </form>
+        <div style={{ marginTop: "10px" }}>
+          <small>
+            By continuing, you agree to Amazon's{" "}
+            <a href="#" style={{ textDecoration: "none" }}>
+              {""}Conditions of use
+            </a>{" "}
+            and {""}
+            <a href="#" style={{ textDecoration: "none" }}>
+              privacy policy{" "}
+            </a>
+          </small>
+        </div>
+      </Box>
+      <div style={{ marginTop: "16px" }}>
+        <Divider>
+          <small style={{ color: "#767676" }}>New to Amazon?</small>
+        </Divider>
+
+        <Link
+          id="register-link"
+          to="/register"
+          style={{ textDecoration: "none", color: "#0000ee" }}
+        >
           <Button
             variant="contained"
-            type="submit"
-            sx={{
-              marginTop: "16px",
+            style={{
+              width: "100%",
+              marginTop: "12px",
               height: "31px",
-              backgroundColor: "#f0c14b",
-              color: "#111111",
-              fontWeight: 700,
-              borderColor: "#a88734 #9c7e31 #846a29",
+              backgroundColor: "#f1f1f1",
+              color: "black",
               textTransform: "none",
-              fontSize: "14px",
-              borderRadius: "4px",
-              "&:hover": {
-                backgroundColor: "#e3a600",
-              },
             }}
           >
             Register
           </Button>
-        </Grid>
-      </form>
-      <div style={{ marginTop: "10px" }}>
-        <small>
-          By creating an account, you agree to Amazon's{" "}
-          <a href="#" style={{ textDecoration: "none" }}>
-            {""}Conditions of use
-          </a>{" "}
-          and {""}
-          <a href="#" style={{ textDecoration: "none" }}>
-            privacy policy{" "}
-          </a>
-        </small>
+        </Link>
       </div>
-      <div style={{ marginTop: "16px" }}>
-        <Divider />
-        <small style={{ color: "#767676" }}>New To Amazon?</small>
-        <Divider />
-        <small>
-          Already have an account?{" "}
-          <Link
-            to="/signin"
-            style={{ textDecoration: "none", color: "#0000ee" }}
-          >
-            Sign in
-          </Link>
-        </small>
-        <div>
-          <small>
-            Buying for work{" "}
-            <a href="#" style={{ textDecoration: "none" }}>
-              {" "}
-              Create a free business account
-            </a>{" "}
-          </small>
-        </div>
-      </div>
-    </Box>
+    </>
   );
 };
 
