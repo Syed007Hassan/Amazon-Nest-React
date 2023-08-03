d1254a1d750fca97fa1f48eff705207f5db140b9
Syed Muhammed Hassan Ali <syed007hassan@gmail.com>
2023-08-03 22:50:07 +0500

    input hook

--- frontend/src/hooks/use-input.ts
+++ frontend/src/hooks/use-input.ts
@@ -1,6 +1,30 @@
+import { InputActionType } from "../models/inputAction";
 import { InputState } from "../models/inputState.interface";
+import { Action } from "../shared/models/action.interface";
 
 const initialInputState: InputState = {
   text: "",
   hasBeenTouched: false,
+};
+
+const inputReducer = (state: InputState, action: Action<InputActionType>) => {
+  switch (action.type) {
+    case "CHANGE":
+      return {
+        ...state,
+        text: action.value,
+      };
+    case "BLUR":
+      return {
+        ...state,
+        hasBeenTouched: true,
+      };
+    case "CLEAR":
+      return {
+        ...state,
+        text: "",
+      };
+    default:
+      return initialInputState;
+  }
 };
